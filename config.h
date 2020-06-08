bb9df0d61176c1e8ca07128439054da4ef9d1831
whatust <whatust@gmail.com>
2020-06-08 16:40:35 -0300

    Changed fonts

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
-static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
+static char *font = "monospace:pixelsize=14:antialias=true:autohint=true";
+static char *font2[] = { "FontAwesome:pixelsize=10:antialias=true:autohint=true" };
 static int borderpx = 3;
 
 /*
